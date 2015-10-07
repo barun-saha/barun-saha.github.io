a57115116c3d2c634bed18ae4749cc813ce1def1
Barun Saha <barun.saha04@gmail.com>
2015-10-07 17:19:34 +0530

    Added some animations in the home page

---
+++ assets/js/animation.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    var selector = "#vls, #se-lite, #dtn-blog, #one-kb";
+    $(selector).css("opacity", "0");
+    $(selector).waypoint({
+        handler: function(direction) {
+            $(this.element).addClass("fadeInLeft");
+        },
+        offset: 'bottom-in-view'
+    });
+    
+    $(".action").css("opacity", "0");
+    $(".action").waypoint({
+        handler: function(direction) {
+            $(this.element).addClass("fadeInDown");
+        },
+        offset: 'bottom-in-view'
+    });
+});
