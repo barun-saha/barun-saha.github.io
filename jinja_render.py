a094e0f03ff225f23df96c632b4ba99071b91053
Barun Saha <barun.saha04@gmail.com>
2015-11-17 16:32:05 +0530

    Added new page for the OMN book

--- jinja_render.py
+++ jinja_render.py
@@ -27,15 +27,15 @@ ENV = Environment(loader=FileSystemLoader('./templates'))
 
 # List of pages to be rendered -- MUST be listed according to their order
 # in the navigation bar
-PAGE_LIST = ['index', 'research', 'projects', 'misc',]
+PAGE_LIST = ['index', 'research', 'projects', 'misc', 'the_omn_book',]
 
 # Title of individual pages
 TITLES = {
-    'contact': 'Barun Saha | Contact',
     'index': 'Barun Saha',
     'misc': 'Barun Saha | Miscellaneous',
     'projects': 'Barun Saha | Projects',
     'research': 'Barun Saha | Research',
+    'the_omn_book': 'Barun Saha | Opportunistic Mobile Networks',
 }
 
 # CSS active class for the navigation bar list items
